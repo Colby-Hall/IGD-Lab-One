39c43e5fac9aa374f25bbc6663f77b736e237b7a
Colby-Hall <hallcc@hendrix.edu>
2019-02-02 12:28:24 -0600

    Working
    Need to add random object spawning

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody ballBody;
+    private int goodPickups;
+    private int badPickups;
+
+    public float speedMult;
+    public Text scoreText;
+    public Text winText;
+    public Text lossText;
+
+
+
+
+    private void Start()
+    {
+        ballBody = GetComponent<Rigidbody>();
+        goodPickups = 0;
+        badPickups = 0;
+        setScore();
+        winText.text = "";
+        lossText.text = "";
+    }
+
+
+
+    private void setScore()
+    {
+        scoreText.text = "Score: " + ((10 * (goodPickups - badPickups)).ToString());
+        if (badPickups >= 3)
+        {
+            lossText.text = "You lost :( Play again? Y/N";
+        }
+        else if (10 * (goodPickups - badPickups) >= 70)
+        {
+            winText.text = "You Win! Play again? Y/N";
+            
+        }
+     }
+
+
+
+    private void FixedUpdate()
+    {
+        float moveHori = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(moveHori, 0, moveVert);
+
+        
+        ballBody.AddForce(movement * speedMult);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (badPickups < 3 && 10 * (goodPickups - badPickups) < 70)
+        {
+            if (other.gameObject.CompareTag("Bad Pickup"))
+            {
+                Destroy(other.gameObject);
+                badPickups += 1;
+                setScore();
+            }
+            else if (other.gameObject.CompareTag("Good Pickup"))
+            {
+                Destroy(other.gameObject);
+                goodPickups += 1;
+                setScore();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Reload.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Reload : MonoBehaviour
+{
+    public Text lossText;
+    public Text winText;
+    
+    void resetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void quitGame()
+    {
+        Application.Quit();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y) && lossText.text == "You lost :( Play again? Y/N")
+        {
+            resetGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.N) && lossText.text == "You lost :( Play again? Y/N")
+        {
+            quitGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.Y) && winText.text == "You Win! Play again? Y/N")
+        {
+            resetGame();
+        }
+        else if (Input.GetKeyDown(KeyCode.N) && winText.text == "You Win! Play again? Y/N") {
+            quitGame();
+        }
+    }
+}
