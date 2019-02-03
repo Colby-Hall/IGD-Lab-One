73ca6639ffcb6592184322bc70f26505bc34f860
Colby-Hall <hallcc@hendrix.edu>
2019-02-02 21:57:30 -0600

    First implementation of random spawning
    Need to implement collision checking on spawned cubes

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject goodPrefab;
+    public GameObject badPrefab;
+
+    private Vector3 goodPosition;
+    private Vector3 badPosition;
+
+    void Start()
+    {
+        
+        for (int i = 0; i < 10; i++)
+        {
+            goodPosition = new Vector3(Random.Range(-14, 13), 1, Random.Range(-13, 13));
+            badPosition = new Vector3(Random.Range(-14, 13), 1, Random.Range(-13, 13));
+
+            Instantiate(goodPrefab, goodPosition, Quaternion.identity);
+            Instantiate(badPrefab, badPosition, Quaternion.identity);
+        }
+    }
+}
